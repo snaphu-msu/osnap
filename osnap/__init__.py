ca2a73cb493e3597f71eb67751ac215dcd3a7f41
johndelker <jdelker@msu.edu>
2026-09-18 10:42:17 -0400

    More planning and setup

---
+++ osnap/__init__.py
@@ -0,0 +1,11 @@
+from importlib import import_module
+
+__all__ = []
+
+
+def __getattr__(name):
+    if name in __all__:
+        module = import_module(f"{__name__}.{name}")
+        globals()[name] = module
+        return module
+    raise AttributeError(name)
